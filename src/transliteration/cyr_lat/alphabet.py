d2bf09b789bca011ddf4444aed0f35628581a43f
Mukhammadsaid19 <m.mamasaidov@student.inha.uz>
2023-01-16 16:47:36 +0500

    Minor fixes with Cyrillic-Latin transliterator

--- src/transliteration/cyr_lat/alphabet.py
+++ src/transliteration/cyr_lat/alphabet.py
@@ -88,33 +88,11 @@ upper_consonants = ["Б", "Д", "Ф", "Г", "Ҳ", "Ж", "К", "Л", "М", "Н", 
 vowels = lower_vowels + upper_vowels
 consonants = lower_consonants + upper_consonants
 
+cyrillic_uppercase = upper_vowels + upper_consonants
+cyrillic_lowercase = lower_vowels + lower_consonants
+
 latin_uppercase = ['’', '‘', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+latin_lowercase = ['’', '‘', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# White space
-white_space = [
-	" ","\u0009",
-          "\u00a0","\u1680",
-          "\u2000","\u2001","\u2002","\u2003","\u2004","\u2005", 
-          "\u2006","\u2007","\u2008","\u2009","\u200a",
-          "\u2028","\u2029","\u202f","\u205f","\u3000"
-		  ];
-
-# Punctuation that ends sentences
-sentence_boundary = ["...",".","?","!","…"];
-
-# Left punctuation
-left_punctuation =   ["(","[","{",
-            "“","‘","‹","«",
-            "'",'%"',"''"];
-
-# Right punctuation - excluding the characters that can be used as apostrophe
-right_punctuation =   sentence_boundary + [",",";",":",
-            ")","]","{",
-            "”","›","»",
-            '%"',"''"];
-
-# Symbols
-symbols = ["-","+","<",">","*","/"];
-
-# Punctuation
-punctuation = right_punctuation + left_punctuation + symbols + white_space
+latin = latin_lowercase + latin_uppercase
+cyrillic = cyrillic_lowercase + cyrillic_uppercase
--- src/transliteration/cyr_lat/model_compile.py
+++ src/transliteration/cyr_lat/model_compile.py
@@ -1,19 +1,6 @@
 import hfst_dev as hfst
-from alphabet import lower_case, upper_case, consonants, vowels, latin_uppercase, punctuation
-
-def cascade(*args):
-	fst = args[0]
-	for tr in args[1:]:
-		fst.compose(tr)
-	return fst
-
-def list_to_group(ls):
-	return "[" + " | ".join(ls) + "]"
-
-def list_to_group_quotes(ls):
-	ls = [f"""\"{l}\"""" for l in ls if l != '%"']
-	ls.append('%"')
-	return "[" + " | ".join(ls) + "]"
+from .alphabet import lower_case, upper_case, consonants, vowels, latin_uppercase, latin, cyrillic
+from .utils import cascade, list_to_group
 
 def ts_rule():
 	ts_rule_1 = hfst.regex(f'[ ц ] -> [ s ], [ Ц ] -> [ S ] || {list_to_group(consonants)} _ , .#. _, " " _ ')
@@ -22,14 +9,21 @@ def ts_rule():
 	return cascade(ts_rule_1, ts_rule_2)
 
 def ye_rule():
-	ye_rule = hfst.regex(f'[ е ] -> [ y e ], Е -> [ Y E ] || {list_to_group(vowels)} _ , .#. _, " " _')
+	ye_rule = hfst.regex(f'[ е ] -> [ y e ], Е -> [ Y е ] || {list_to_group(vowels)} _ , .#. _, " " _')
 	return ye_rule
 
 
 def normalize_digraph_case():
-	# print(punctuation)
-	normalize_h = hfst.regex(f'[ h ] -> [ H ] || [{list_to_group_quotes(punctuation)} | {list_to_group(latin_uppercase)} | .#. ] [ C | S ] _ [{list_to_group_quotes(punctuation)} | {list_to_group(latin_uppercase)} | .#. ]')
-	return normalize_h
+	# If Sh is the ending of the word, then it must be SH
+	normalize_h_1 = hfst.regex(f'[ h ] -> [ H ] || [ C | S ] _ \[{list_to_group(latin + cyrillic)}]')
+	# If Sh is followed by any uppercase Latin letter, then it must be SH
+	normalize_h_2 = hfst.regex(f'[ h ] -> [ H ] || [ C | S ] _ [{list_to_group(latin_uppercase)}]')
+
+	# Same rule for Ye, Yo, Yu, Ya
+	normalize_vowel_1 = hfst.regex(f'o -> O, e -> E, u -> U, a -> A || Y _ \{list_to_group(latin + cyrillic)}')
+	normalize_vowel_2 = hfst.regex(f'o -> O, e -> E, u -> U, a -> A || Y _ {list_to_group(latin_uppercase)}')
+
+	return cascade(normalize_h_1, normalize_h_2, normalize_vowel_1, normalize_vowel_2)
 
 
 def model_compile():
